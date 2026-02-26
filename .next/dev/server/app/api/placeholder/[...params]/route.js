ad0174673a52630b7f3f1f9027e5697b4473934d
hackerug06wizard <hackerugg06@gmail.com>
2026-02-26 07:16:16 +0300

    Create route.js

---
+++ .next/dev/server/app/api/placeholder/[...params]/route.js
@@ -0,0 +1,6 @@
+var R=require("../../../../chunks/[turbopack]_runtime.js")("server/app/api/placeholder/[...params]/route.js")
+R.c("server/chunks/node_modules_next_6eb0936a._.js")
+R.c("server/chunks/[root-of-the-server]__d6ed9abb._.js")
+R.c("server/chunks/_next-internal_server_app_api_placeholder_[___params]_route_actions_cc82a108.js")
+R.m("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/src/app/api/placeholder/[...params]/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)")
+module.exports=R.m("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/src/app/api/placeholder/[...params]/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)").exports
